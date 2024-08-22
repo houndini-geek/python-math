05cfeed1a463373deec78016d5052daba54580fc
HoundiX <sanonloick0@gmail.com>
2024-08-22 10:17:21 -0400

    Two version of the codes

--- python-math.py
+++ python-math.py
@@ -49,5 +49,61 @@ while True:
          print("Error: Please enter a valid number.")  # Friendly message for invalid input
         
         
-         
+
+
+#Refractored codes
+
+#     def doSum(val1, val2):
+#     """Returns the sum of two numbers."""
+#      return val1 + val2
+
+# def doSub(val1, val2):
+#     """Returns the difference between two numbers."""
+#     return val1 - val2
+
+# def doMul(val1, val2):
+#     """Returns the product of two numbers."""
+#     return val1 * val2
+
+# def doDiv(val1, val2):
+#     """Returns the division of two numbers, ensuring no division by zero."""
+#     if val2 != 0:
+#         return val1 / val2
+#     else:
+#         return "undefined (division by zero is not allowed)"
+
+# def get_numbers():
+#     """Prompts the user for two numeric inputs and returns them as floats."""
+#     while True:
+#         try:
+#             val1 = float(input('Enter the first value: '))
+#             val2 = float(input('Enter the second value: '))
+#             return val1, val2
+#         except ValueError:
+#             print("Error: Please enter a valid number.")
+
+# # Main Program
+# while True: 
+#     query = input(f'What operation do you want to do? {oprts}, Enter "exit" to close the program: ')
+
+#     if query == 'exit':
+#         print('Exiting the program. Goodbye!')
+#         break
+    
+#     if query not in oprts:
+#         print('Invalid operation selected. Please choose from the given list.')
+#     else:
+#         val1, val2 = get_numbers()
+
+#         if query == 'addition': 
+#             result = doSum(val1, val2)
+#         elif query == 'subtraction':
+#             result = doSub(val1, val2)
+#         elif query == 'multiplication':
+#             result = doMul(val1, val2)
+#         elif query == 'division':
+#             result = doDiv(val1, val2)
+
+#         print(f'The result of {query} is: {result}')
+
     
